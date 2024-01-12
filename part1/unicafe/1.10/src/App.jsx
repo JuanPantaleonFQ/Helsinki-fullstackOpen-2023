6443ec2e173e358f946f1031a05965dd1a1aa78d
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2024-01-12 14:02:05 +0000

    cambios

--- part1/unicafe/1.10/src/App.jsx
+++ part1/unicafe/1.10/src/App.jsx
@@ -1,34 +1,67 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const Display =({text})=>{  
   return (
-    <>
+    <p>{text}</p>
+  )
+}
+const StatisticLine = ({text,value})=>{
+  return(<p>{text} {value}</p>)    
+}
+
+const DisplayStatistics = (props) => {
+  if(props.total == 0 ){
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Statistics</p>
+        <p>No feedback given</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
+  }else{
+
+  }
+  return(
+    <div>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+      
+    </div>
+  )
+}
+
+
+
+function App() {  
+  //Functions used for handle events of buttons
+  const [goodState,setGoodState]= useState(0)
+  const [neutralState,setNeutralState]= useState(0)
+  const [badState,setBadState]= useState(0)
+  const CalculateTotal = goodState +neutralState + badState
+  const calculateAverage = (goodState-badState)/CalculateTotal
+  const calculatePositive = (goodState/CalculateTotal) *100
+  
+  const handleGood = () =>{
+    setGoodState(goodState+1)
+  }
+  const handleNeutral = () =>{
+    setNeutralState(neutralState+1)
+  }
+  const handleBad = () =>{
+    setBadState(badState+1)
+  }
+   
+  return (
+    <div>
+      
+      <Display text={"Give feed back"}/>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
+      <DisplayStatistics good={goodState} neutral={neutralState} bad={badState} total={CalculateTotal} average={calculateAverage} positive={calculatePositive}/>
+    </div>
   )
 }
 
