3368592c37b43ba98cb820b38f34c4ca01988cf7
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2024-01-22 21:03:24 +0000

    2.2 done

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -5,12 +5,13 @@ const ParagrafWritter = ({ prop , number }) => <p>{prop} {number}</p>
 
 
 const ContentWritter = ({ part }) => {
-    console.log(part)
+   const total = part[0].exercises + part[1].exercises + part[2].exercises
     return(
         <div>
             <ParagrafWritter prop={part[0].name} number={part[0].exercises}/>
             <ParagrafWritter prop={part[1].name} number={part[1].exercises}/>
             <ParagrafWritter prop={part[2].name} number={part[2].exercises}/>
+            <ParagrafWritter prop="Total of" number={total}/>
         </div>
     )
 }
