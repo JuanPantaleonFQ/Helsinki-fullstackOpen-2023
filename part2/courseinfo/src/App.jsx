9c3fa0f0c9698206558cd279378d63afdbbb1ea9
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2024-01-22 20:16:10 +0000

    Part 2 commited

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Note from './components/Course'
 
-function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <div></div>      
-  )
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
 }
 
+
+
 export default App
+
---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+const Course = ({ course }) => {
+
+}
+export default Course
